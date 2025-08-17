52f548e17757fcb31d3a3251a4e6752104d484e0
FuzzyKiwi3 <collin.greco@gmail.com>
2025-08-17 15:49:21 -0400

    Initial commit

---
+++ src/main/java/fuzzykiwi3/paleontologyunearthed/PaleontologyUnearthedClient.java
@@ -0,0 +1,11 @@
+package fuzzykiwi3.paleontologyunearthed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaleontologyUnearthedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/fuzzykiwi3/paleontologyunearthed/PaleontologyUnearthedDataGenerator.java
@@ -0,0 +1,11 @@
+package fuzzykiwi3.paleontologyunearthed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PaleontologyUnearthedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
