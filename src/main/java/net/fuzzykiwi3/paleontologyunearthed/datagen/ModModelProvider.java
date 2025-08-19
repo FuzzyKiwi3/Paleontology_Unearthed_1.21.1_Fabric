17555accdb8ceb4ebadbb2ff66bda3963dfac453
FuzzyKiwi3 <collin.greco@gmail.com>
2025-08-19 01:03:31 -0400

    Got nowhere trying to add chiseling animations. At least the sounds work...

--- src/main/java/net/fuzzykiwi3/paleontologyunearthed/datagen/ModModelProvider.java
+++ src/main/java/net/fuzzykiwi3/paleontologyunearthed/datagen/ModModelProvider.java
@@ -3,7 +3,9 @@ package net.fuzzykiwi3.paleontologyunearthed.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fuzzykiwi3.paleontologyunearthed.item.ModItems;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.client.*;
+import net.minecraft.util.Identifier;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
--- src/main/java/net/fuzzykiwi3/paleontologyunearthed/item/custom/ChiselItem.java
+++ src/main/java/net/fuzzykiwi3/paleontologyunearthed/item/custom/ChiselItem.java
@@ -3,7 +3,6 @@ package net.fuzzykiwi3.paleontologyunearthed.item.custom;
 import net.fuzzykiwi3.paleontologyunearthed.sound.ModSounds;
 import net.fuzzykiwi3.paleontologyunearthed.util.ModTags;
 import net.minecraft.block.*;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,32 +15,27 @@ import net.minecraft.registry.Registries;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Arm;
-import net.minecraft.util.Hand;
-import net.minecraft.util.UseAction;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 
 import java.util.Map;
 
 public class ChiselItem extends ToolItem {
     private static final int MAX_CHISEL_TIME = 200;
-    private ToolMaterial CHISEL_MATERIAL;
-    private float CHISEL_PROGRESS = 0;
-    private float COOLDOWN_TICKS = 0;
-    private BlockPos PREVIOUS_BLOCK_POS = null;
+    private static final int SOUND_INTERVAL = 15; // ticks between chisel sounds
+    private static final int COOLDOWN_TICKS = 10;
 
-    // Help from KaupenJoe's 1.21 Fabric Modding tutorial
+    private final ToolMaterial material;
+    private float progress = 0;
+    private int cooldown = 0;
+    private BlockPos lastBlockPos = null;
+    private int soundTickCounter = 0;
 
     private static final Map<Block, Block> CHISEL_MAP = Map.of(
-            // Make sure to add blocks to the tag
             Blocks.STONE_BRICKS, Blocks.CHISELED_STONE_BRICKS,
             Blocks.COBBLED_DEEPSLATE, Blocks.CHISELED_DEEPSLATE,
             Blocks.TUFF, Blocks.CHISELED_TUFF,
@@ -55,66 +49,26 @@ public class ChiselItem extends ToolItem {
 
     public ChiselItem(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings);
-
-        CHISEL_MATERIAL = toolMaterial;
+        this.material = toolMaterial;
     }
 
-    private BlockPos getPreviousBlockPos() {
-        return PREVIOUS_BLOCK_POS;
+    private float getProgressPerTick(Block block) {
+        // Increase float to increase chisel time
+        return block.getDefaultState().isIn(ModTags.Blocks.IS_CHISELABLE)
+                ? (material.getMiningSpeedMultiplier() / block.getHardness()) / 90f
+                : 0;
     }
 
-    private void setPreviousBlockPos(BlockPos value) {
-        PREVIOUS_BLOCK_POS = value;
+    private boolean isComplete() {
+        return progress >= 1;
     }
-
-    private void resetPreviousBlockPos() {
-        PREVIOUS_BLOCK_POS = null;
-    }
-
-    private void startChiselCooldown() {
-        COOLDOWN_TICKS = 10;
-    }
-
-    private void reduceCooldown(int reduction) {
-        COOLDOWN_TICKS -= reduction;
-    }
-
-    private boolean onCooldown() {
-        return COOLDOWN_TICKS > 0;
-    }
-
-    private void setChiselProgress(float newValue) {
-        CHISEL_PROGRESS = newValue;
-    }
-
-    private float getChiselProgress() {
-        return CHISEL_PROGRESS;
-    }
-
-    private void increaseChiselProgress(float increaseAmount) {
-        setChiselProgress(getChiselProgress() + increaseAmount);
-    }
-
-    private float getChiselProgressPerTick(Block block) {
-        if (block.getDefaultState().isIn(ModTags.Blocks.IS_CHISELABLE)) {
-            return (CHISEL_MATERIAL.getMiningSpeedMultiplier() / block.getHardness()) / 30;
-        } else {
-            return 0;
-        }
-    }
-
-    private boolean chiselingIsComplete() {
-        return getChiselProgress() >= 1;
-    }
-
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        PlayerEntity playerEntity = context.getPlayer();
-        if (playerEntity != null && this.getHitResult(playerEntity).getType() == HitResult.Type.BLOCK) {
-            playerEntity.setCurrentHand(context.getHand());
+        PlayerEntity player = context.getPlayer();
+        if (player != null && getHitResult(player).getType() == HitResult.Type.BLOCK) {
+            player.setCurrentHand(context.getHand());
         }
-
         return ActionResult.CONSUME;
     }
 
@@ -130,104 +84,123 @@ public class ChiselItem extends ToolItem {
 
     @Override
     public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
-        if (remainingUseTicks >= 0 && user instanceof PlayerEntity playerEntity && !onCooldown()) {
-            HitResult hitResult = this.getHitResult(playerEntity);
-            if (hitResult instanceof BlockHitResult blockHitResult && hitResult.getType() == HitResult.Type.BLOCK) {
-                int i = this.getMaxUseTime(stack, user) - remainingUseTicks + 1;
-                boolean bl = i % 10 == 5;
-                if (bl) {
-                    BlockPos blockPos = blockHitResult.getBlockPos();
-                    if (getPreviousBlockPos() == null
-                        || (blockPos.getX() != getPreviousBlockPos().getX()
-                            || blockPos.getY() != getPreviousBlockPos().getY()
-                            || blockPos.getZ() != getPreviousBlockPos().getZ())) {
-                        setChiselProgress(0);
-                    }
-                    setPreviousBlockPos(blockPos);
-                    BlockState blockState = world.getBlockState(blockPos);
-                    Block block = blockState.getBlock();
-                    Arm arm = user.getActiveHand() == Hand.MAIN_HAND ? playerEntity.getMainArm() : playerEntity.getMainArm().getOpposite();
-                    if (blockState.hasBlockBreakParticles() && blockState.getRenderType() != BlockRenderType.INVISIBLE) {
-                        this.addDustParticles(world, blockHitResult, blockState, user.getRotationVec(0.0F), arm);
-                    }
+        if (!(user instanceof PlayerEntity player)) {
+            resetChisel(user);
+            return;
+        }
 
-                    if (blockState.getBlock().getDefaultState().isIn(ModTags.Blocks.IS_CHISELABLE)) {
-                        world.playSound(playerEntity, blockPos, ModSounds.CHISEL_USE, SoundCategory.BLOCKS);
-                    }
+        HitResult hitResult = getHitResult(player);
+        if (!(hitResult instanceof BlockHitResult blockHit) || hitResult.getType() != HitResult.Type.BLOCK) {
+            resetChisel(user);
+            return;
+        }
 
-                    if (CHISEL_MAP.containsKey(block) && block.getDefaultState().isIn(ModTags.Blocks.IS_CHISELABLE)
-                        && !world.isClient()) {
-                        increaseChiselProgress(getChiselProgressPerTick(block));
+        BlockPos pos = blockHit.getBlockPos();
+        BlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
 
-                        if (chiselingIsComplete()) {
-                            if (user instanceof ServerPlayerEntity serverPlayer) {
-                                serverPlayer.networkHandler.sendPacket(new PlaySoundS2CPacket(
-                                        Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_GRINDSTONE_USE), SoundCategory.BLOCKS,
-                                        blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5,
-                                        1.0f, 1.0f, 0L));
-                            }
-                            world.setBlockState(blockPos, CHISEL_MAP.get(block).getDefaultState());
-                            stack.damage(1, ((ServerWorld) world), ((ServerPlayerEntity) user),
-                                    item -> user.sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
-                            setChiselProgress(0);
-                            resetPreviousBlockPos();
-                            startChiselCooldown();
-                            user.stopUsingItem();
-                        }
+        boolean blockIsChiselable = state.isIn(ModTags.Blocks.IS_CHISELABLE) && CHISEL_MAP.containsKey(block);
+
+        // Reset progress only if we moved to a new block
+        if (lastBlockPos == null || !lastBlockPos.equals(pos)) {
+            if (lastBlockPos != null
+                && !world.getBlockState(lastBlockPos).isIn(ModTags.Blocks.IS_CHISELABLE)
+                && blockIsChiselable) {
+                soundTickCounter = 0;
+            }
+            progress = 0;
+        }
+        lastBlockPos = pos;
+
+        if (blockIsChiselable && soundTickCounter == 0) {
+            addDustParticles(world, blockHit, state, user.getRotationVec(0), player.getMainArm());
+        }
+
+        // Chiseling Progress & Completion
+        if (blockIsChiselable && !world.isClient()) {
+            progress += getProgressPerTick(block);
+
+            if (progress < 1) {
+                if (soundTickCounter == 0) {
+                    if (user instanceof ServerPlayerEntity serverPlayer) {
+                        serverPlayer.networkHandler.sendPacket(new PlaySoundS2CPacket(
+                                Registries.SOUND_EVENT.getEntry(ModSounds.CHISEL_USE),
+                                SoundCategory.BLOCKS,
+                                pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                                1.0f, 1.0f, 0L));
                     }
                 }
-            } else {
-                setChiselProgress(0);
-                user.stopUsingItem();
+                soundTickCounter++;
+                if (soundTickCounter >= SOUND_INTERVAL) {
+                    soundTickCounter = 0;
+                }
             }
-        } else {
-            reduceCooldown(1);
-            setChiselProgress(0);
-            user.stopUsingItem();
+
+            else if (progress >= 1) {
+                // Play only the grindstone completion sound
+                if (user instanceof ServerPlayerEntity serverPlayer) {
+                    serverPlayer.networkHandler.sendPacket(new PlaySoundS2CPacket(
+                            Registries.SOUND_EVENT.getEntry(SoundEvents.BLOCK_GRINDSTONE_USE),
+                            SoundCategory.BLOCKS,
+                            pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                            1.0f, 1.0f, 0L));
+                }
+
+                world.setBlockState(pos, CHISEL_MAP.get(block).getDefaultState());
+                stack.damage(1, (ServerWorld) world, (ServerPlayerEntity) user,
+                        item -> user.sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
+
+                resetChisel(user);
+            }
         }
+
+        // Decrease cooldown if active
+        if (cooldown > 0) cooldown--;
+    }
+
+    /** Unified reset for progress, cooldown, and sound */
+    private void resetChisel(LivingEntity user) {
+        progress = 0;
+        soundTickCounter = 0;
+        lastBlockPos = null;
+        cooldown = COOLDOWN_TICKS;
+        user.stopUsingItem();
     }
 
     private HitResult getHitResult(PlayerEntity user) {
-        return ProjectileUtil.getCollision(user, entity -> !entity.isSpectator() && entity.canHit(), user.getBlockInteractionRange());
+        return ProjectileUtil.getCollision(user,
+                e -> !e.isSpectator() && e.canHit(),
+                user.getBlockInteractionRange());
     }
 
-    private void addDustParticles(World world, BlockHitResult hitResult, BlockState state, Vec3d userRotation, Arm arm) {
-        double d = 3.0;
-        int i = arm == Arm.RIGHT ? 1 : -1;
-        int j = world.getRandom().nextBetweenExclusive(7, 12);
-        BlockStateParticleEffect blockStateParticleEffect = new BlockStateParticleEffect(ParticleTypes.BLOCK, state);
-        Direction direction = hitResult.getSide();
-        DustParticlesOffset dustParticlesOffset = DustParticlesOffset.fromSide(userRotation, direction);
-        Vec3d vec3d = hitResult.getPos();
+    private void addDustParticles(World world, BlockHitResult hit, BlockState state, Vec3d rotation, Arm arm) {
+        int directionMultiplier = arm == Arm.RIGHT ? 1 : -1;
+        int count = world.getRandom().nextBetweenExclusive(7, 12); // number of particles per sound
+        BlockStateParticleEffect effect = new BlockStateParticleEffect(ParticleTypes.BLOCK, state);
+        Direction side = hit.getSide();
+        DustParticlesOffset offset = DustParticlesOffset.fromSide(rotation, side);
+        Vec3d pos = hit.getPos();
 
-        for (int k = 0; k < j; k++) {
-            world.addParticle(
-                    blockStateParticleEffect,
-                    vec3d.x - (direction == Direction.WEST ? 1.0E-6F : 0.0F),
-                    vec3d.y,
-                    vec3d.z - (direction == Direction.NORTH ? 1.0E-6F : 0.0F),
-                    dustParticlesOffset.xd() * i * 3.0 * world.getRandom().nextDouble(),
-                    0.0,
-                    dustParticlesOffset.zd() * i * 3.0 * world.getRandom().nextDouble()
-            );
+        for (int k = 0; k < count; k++) {
+            world.addParticle(effect,
+                    pos.x - (side == Direction.WEST ? 1e-6 : 0),
+                    pos.y,
+                    pos.z - (side == Direction.NORTH ? 1e-6 : 0),
+                    offset.xd() * directionMultiplier * 3 * world.getRandom().nextDouble(),
+                    0,
+                    offset.zd() * directionMultiplier * 3 * world.getRandom().nextDouble());
         }
     }
 
     record DustParticlesOffset(double xd, double yd, double zd) {
-        private static final double field_42685 = 1.0;
-        private static final double field_42686 = 0.1;
-
-        public static DustParticlesOffset fromSide(Vec3d userRotation, Direction side) {
-            double d = 0.0;
-
+        public static DustParticlesOffset fromSide(Vec3d rotation, Direction side) {
             return switch (side) {
-                case DOWN, UP -> new DustParticlesOffset(userRotation.getZ(), 0.0, -userRotation.getX());
-                case NORTH -> new DustParticlesOffset(1.0, 0.0, -0.1);
-                case SOUTH -> new DustParticlesOffset(-1.0, 0.0, 0.1);
-                case WEST -> new DustParticlesOffset(-0.1, 0.0, -1.0);
-                case EAST -> new DustParticlesOffset(0.1, 0.0, 1.0);
+                case DOWN, UP -> new DustParticlesOffset(rotation.getZ(), 0, -rotation.getX());
+                case NORTH -> new DustParticlesOffset(1, 0, -0.1);
+                case SOUTH -> new DustParticlesOffset(-1, 0, 0.1);
+                case WEST -> new DustParticlesOffset(-0.1, 0, -1);
+                case EAST -> new DustParticlesOffset(0.1, 0, 1);
             };
         }
     }
 }
-
